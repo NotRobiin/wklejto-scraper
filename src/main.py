6b61098f9bfcd04c978391d542248c166bad73a9
Wicked <wwicked@o2.pl>
2020-11-19 16:07:59 +0100

    Add start parameters

--- src/main.py
+++ src/main.py
@@ -1,10 +1,24 @@
 from scraper import Scraper
 from config import Config
 from mongo import Database
+import sys
+
+
+def get_range() -> list:
+    args = sys.argv
+
+    if len(args) != 2:
+        return [0, 0]
+
+    return [int(x) for x in args[-1].split("-")]
 
 
 def main() -> None:
     cfg = Config()
+    r = get_range()
+
+    if r[0] and r[1]:
+        cfg.RANGE = range(r[0], r[1])
 
     db = Database(cfg)
     db.start(confirm=True)
